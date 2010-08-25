c54c48b98467c100dbb36744eca1d874be076722
Johnny Khai Nguyen <johnnyn@gmail.com>
2010-08-25 13:52:15 -0500

    tweak module; remove self

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ Simple linkups for mentions (words that start with "@", like Twitter replies)
 
 # Usage
 
-    >> Mentions.linkup("@Johnny Lorem ipsum dolor sit amet, consectetur adipisicing elit", '/users/\1', '_blank')
+		>> include Mentions
+
+    >> linkup("@Johnny Lorem ipsum dolor sit amet, consectetur adipisicing elit", '/users/\1', '_blank')
     => "@<a href='/users/johnny' target='_blank'>johnny</a> Lorem ipsum dolor sit amet, consectetur adipisicing elit"  
 
 Copyright (c) 2009 Johnny Khai Nguyen, released under the MIT license
--- test/mentions_test.rb
+++ test/mentions_test.rb
@@ -1,18 +1,19 @@
 require 'helper'
 
 class MentionsTest < Test::Unit::TestCase
+  include Mentions
 
   context "While linking mentions" do
     
     should "linkup a specific text" do
       text = '@johnny Lorem ipsum dolor sit amet, consectetur adipisicing elit'
-      link = Mentions.linkup(text, '/users/\1', '_blank')
+      link = linkup(text, '/users/\1', '_blank')
       link.should == '@<a href="/users/johnny" target="_blank">johnny</a> Lorem ipsum dolor sit amet, consectetur adipisicing elit'
     end
     
     should "linkup a to Twitter" do
       text = '@johnny_nguyen Lorem ipsum dolor sit amet, consectetur adipisicing elit'
-      link = Mentions.linkup(text, 'http://twitter.com/\1', '_blank')
+      link = linkup(text, 'http://twitter.com/\1', '_blank')
       link.should == '@<a href="http://twitter.com/johnny_nguyen" target="_blank">johnny_nguyen</a> Lorem ipsum dolor sit amet, consectetur adipisicing elit'
     end
   end
